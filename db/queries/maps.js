72fa1ded1f3db3c1d717455ffc6c15fea830efc7
Lisa Wild <lisakwild@gmail.com>
2024-04-25 12:40:58 -0700

    feat: add function to pull a single map (via the maps id) from the database

--- db/queries/maps.js
+++ db/queries/maps.js
@@ -4,4 +4,19 @@ const getAllMaps = () => {
   return Promise.resolve('aw yeah')
 };
 
-module.exports = { getAllMaps };
+const getSingleMap = (mapID) => {
+  const queryParams = [mapID];
+  const queryString = `
+  SELECT * FROM maps JOIN users ON owner_id = users.id WHERE maps.id = $1;`;
+
+  db.query(queryString, queryParams)
+  .then((results) => {
+    let mapObj = results.rows[0];
+    return mapObj;
+  })
+  .catch((err) => {
+    return err.message
+  }
+)};
+
+module.exports = { getAllMaps, getSingleMap };
