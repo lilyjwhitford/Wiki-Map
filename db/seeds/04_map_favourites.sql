97947dc5a1379a1fc1b60e75f6794cdb51da393c
Lisa Wild <lisakwild@gmail.com>
2024-04-26 11:18:08 -0700

    chore: Add seeds for markers and map_favourites

---
+++ db/seeds/04_map_favourites.sql
@@ -0,0 +1,9 @@
+-- map favourites table seeds here 
+INSERT INTO map_favourites (user_id, map_id) VALUES (1, 5);
+INSERT INTO map_favourites (user_id, map_id) VALUES (1, 3);
+INSERT INTO map_favourites (user_id, map_id) VALUES (1, 4);
+INSERT INTO map_favourites (user_id, map_id) VALUES (2, 1);
+INSERT INTO map_favourites (user_id, map_id) VALUES (3, 4);
+INSERT INTO map_favourites (user_id, map_id) VALUES (4, 1);
+INSERT INTO map_favourites (user_id, map_id) VALUES (5, 1);
+
