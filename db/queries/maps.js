02b38509bd1f6d5d2a4694c5353c81827ea89dde
Lisa Wild <lisakwild@gmail.com>
2024-04-26 17:32:22 -0700

    bugfix: add return before db.query as you must always return a promise

--- db/queries/maps.js
+++ db/queries/maps.js
@@ -3,14 +3,13 @@ const db = require('../connection');
 const getAllMaps = () => {
   const queryString = `SELECT * FROM maps JOIN users ON owner_id = users.id;`;
 
-  db.query(queryString)
+  return db.query(queryString)
   .then(results => {
     const mapsObj = results.rows;
-    console.log(mapsObj);
     return mapsObj;
   })
   .catch((err) => {
-    return err.message
+    return err.message;
   })
 };
 
@@ -22,13 +21,13 @@ const getSingleMap = (mapID) => {
   JOIN users ON owner_id = users.id 
   WHERE maps.id = $1;`;
 
-  db.query(queryString, queryParams)
+  return db.query(queryString, queryParams)
   .then(results => {
     const mapObj = results.rows[0];
     return mapObj;
   })
   .catch((err) => {
-    return err.message
+    return err.message;
   }
 )};
 
