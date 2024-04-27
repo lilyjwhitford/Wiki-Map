8ce8f66164c9ef4c6b3eef8f3d436d32b60f831d
PeterDevelops <PeterDinhBC@gmail.com>
2024-04-27 15:25:48 -0700

    refactor: update maps and markers ERD

--- db/schema/03_markers.sql
+++ db/schema/03_markers.sql
@@ -5,9 +5,9 @@ CREATE TABLE markers (
   id SERIAL PRIMARY KEY NOT NULL,
   creator_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
   map_id INTEGER REFERENCES maps(id) ON DELETE CASCADE,
-  x_coord INTEGER NOT NULL,
-  y_coord INTEGER NOT NULL,
-  marker_title VARCHAR(255) NOT NULL,
-  marker_description VARCHAR(140) NOT NULL,
-  marker_image VARCHAR(255) NOT NULL
+  lat INTEGER NOT NULL,
+  long INTEGER NOT NULL,
+  title VARCHAR(255) NOT NULL,
+  description VARCHAR(140) NOT NULL,
+  image VARCHAR(255) NOT NULL
 );
