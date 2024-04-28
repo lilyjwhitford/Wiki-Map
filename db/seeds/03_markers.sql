e08f9387bbb0122d8edf253ac75a8514aaeba094
Lisa Wild <lisakwild@gmail.com>
2024-04-28 12:10:11 -0700

    bugfix: add missing comma after lat

--- db/seeds/03_markers.sql
+++ db/seeds/03_markers.sql
@@ -1,31 +1,31 @@
 -- markers table seeds here
 
-INSERT INTO markers (creator_id, map_id, lat long, title, description, image_url)
+INSERT INTO markers (creator_id, map_id, lat, long, title, description, image_url)
 VALUES (1, 1, -123.116944, 49.281389, 'Tojos Restaurant', 'Upscale Sushi Restaurant', 'images/restaurant.jpg');
 
-INSERT INTO markers (creator_id, map_id, lat long, title, description, image_url)
+INSERT INTO markers (creator_id, map_id, lat, long, title, description, image_url)
 VALUES (1, 1, -123.102222, 49.287222, 'Miku Restaurant', 'Upscale Sushi Restaurant', 'images/restaurant.jpg');
 
-INSERT INTO markers (creator_id, map_id, lat long, title, description, image_url)
+INSERT INTO markers (creator_id, map_id, lat, long, title, description, image_url)
 VALUES (1, 1, -123.182778, 49.194167, 'Aburi Restaurants (Richmond)', 'Upscale Sushi Restaurant', 'images/restaurant.jpg');
 
-INSERT INTO markers (creator_id, map_id, lat long, title, description, image_url)
+INSERT INTO markers (creator_id, map_id, lat, long, title, description, image_url)
 VALUES (1, 1, -122.986389, 49.278333, 'Kintaro Sushi Restaurant', 'Upscale Sushi Restaurant', 'images/restaurant.jpg');
 
-INSERT INTO markers (creator_id, map_id, lat long, title, description, image_url)
+INSERT INTO markers (creator_id, map_id, lat, long, title, description, image_url)
 VALUES (1, 1, -123.083333, 49.311389, 'Tomokazu Sushi Restaurant', 'Upscale Sushi Restaurant', 'images/restaurant.jpg');
 
-INSERT INTO markers (creator_id, map_id, lat long, title, description, image_url)
+INSERT INTO markers (creator_id, map_id, lat, long, title, description, image_url)
 VALUES (1, 1, -122.909722, 49.200556, 'Sushi Mura', 'Upscale Sushi Restaurant', 'images/restaurant.jpg');
 
-INSERT INTO markers (creator_id, map_id, lat long, title, description, image_url)
+INSERT INTO markers (creator_id, map_id, lat, long, title, description, image_url)
 VALUES (1, 1, -122.883056, 49.281389, 'Sushi Town', 'Upscale Sushi Restaurant', 'images/restaurant.jpg');
 
-INSERT INTO markers (creator_id, map_id, lat long, title, description, image_url)
+INSERT INTO markers (creator_id, map_id, lat, long, title, description, image_url)
 VALUES (1, 1, -123.040278, 49.123889, 'Sushi Garden', 'Upscale Sushi Restaurant', 'images/restaurant.jpg');
 
-INSERT INTO markers (creator_id, map_id, lat long, title, description, image_url)
+INSERT INTO markers (creator_id, map_id, lat, long, title, description, image_url)
 VALUES (1, 1, -122.806389, 49.280278, 'Sushi Mori', 'Upscale Sushi Restaurant', 'images/restaurant.jpg');
 
-INSERT INTO markers (creator_id, map_id, lat long, title, description, image_url)
+INSERT INTO markers (creator_id, map_id, lat, long, title, description, image_url)
 VALUES (1, 1, -122.847222, 49.190833, 'Sushi Can', 'Upscale Sushi Restaurant', 'images/restaurant.jpg');
