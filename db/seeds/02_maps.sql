cc3d3306fbadb0947b39b3b763137f1071275e85
Lisa Wild <lisakwild@gmail.com>
2024-04-29 09:43:13 -0700

    chore: make long & lat unique and add real photos

--- db/seeds/02_maps.sql
+++ db/seeds/02_maps.sql
@@ -1,15 +1,15 @@
 -- maps table seeds here (Example)
 INSERT INTO maps (owner_id, title, description, image_url, lat, long)
-VALUES (1, 'Best Sushi Spots', 'my favourite sushi places to bring my friends', 'https://byfood.b-cdn.net/api/public/assets/22821/content?optimizer=image', -123.116944, 49.281389);
+VALUES (1, 'Best Sushi Spots', 'my favourite sushi places to bring my friends', 'https://images.unsplash.com/photo-1521722210400-244bfa6d1b1b', 34.052235, -118.243683);
 
 INSERT INTO maps (owner_id, title, description, image_url, lat, long)
-VALUES (2, 'Top Hiking Trails', 'My favorite trails for a great adventure', 'https://example.com/images/hiking-trails.jpg', -123.116944, 49.281389);
+VALUES (2, 'Top Hiking Trails', 'My favorite trails for a great adventure', 'https://images.unsplash.com/photo-1518791841217-8f162f1e1135', 51.5074, -0.1278);
 
 INSERT INTO maps (owner_id, title, description, image_url, lat, long)
-VALUES (3, 'Best Coffee Shops', 'Spots for a cozy coffee break', 'https://example.com/images/coffee-shops.jpg', -123.116944, 49.281389);
+VALUES (3, 'Best Coffee Shops', 'Spots for a cozy coffee break', 'https://images.unsplash.com/photo-1514888286974-6c03e2ca1dba', 40.7128, -74.0060);
 
 INSERT INTO maps (owner_id, title, description, image_url, lat, long)
-VALUES (4, 'Local Art Galleries', 'Discovering new artists and artworks', 'https://example.com/images/art-galleries.jpg', -123.116944, 49.281389);
+VALUES (4, 'Local Art Galleries', 'Discovering new artists and artworks', 'https://images.unsplash.com/photo-1536054106160-8a1b3a3e3e59', 45.5017, -73.5673);
 
 INSERT INTO maps (owner_id, title, description, image_url, lat, long)
-VALUES (5, 'Ski Resorts', 'Snowy escapes for winter fun', 'https://example.com/images/ski-resorts.jpg', -123.116944, 49.281389);
+VALUES (5, 'Ski Resorts', 'Snowy escapes for winter fun', 'https://images.unsplash.com/photo-1520828697294-361b5f3afbe6', 49.2827, -123.1207);
