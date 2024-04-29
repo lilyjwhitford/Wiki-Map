134fc898237831abe730ca2285418398fcb0d5b6
Lisa Wild <lisakwild@gmail.com>
2024-04-29 08:57:49 -0700

    bugfix: correct column names to match the ERD & db

--- db/seeds/02_maps.sql
+++ db/seeds/02_maps.sql
@@ -1,15 +1,15 @@
 -- maps table seeds here (Example)
-INSERT INTO maps (owner_id, title, map_description, map_image, lat, long)
+INSERT INTO maps (owner_id, title, description, image_url, lat, long)
 VALUES (1, 'Best Sushi Spots', 'my favourite sushi places to bring my friends', 'https://byfood.b-cdn.net/api/public/assets/22821/content?optimizer=image', -123.116944, 49.281389);
 
-INSERT INTO maps (owner_id, title, map_description, map_image, lat, long)
+INSERT INTO maps (owner_id, title, description, image_url, lat, long)
 VALUES (2, 'Top Hiking Trails', 'My favorite trails for a great adventure', 'https://example.com/images/hiking-trails.jpg', -123.116944, 49.281389);
 
-INSERT INTO maps (owner_id, title, map_description, map_image, lat, long)
+INSERT INTO maps (owner_id, title, description, image_url, lat, long)
 VALUES (3, 'Best Coffee Shops', 'Spots for a cozy coffee break', 'https://example.com/images/coffee-shops.jpg', -123.116944, 49.281389);
 
-INSERT INTO maps (owner_id, title, map_description, map_image, lat, long)
+INSERT INTO maps (owner_id, title, description, image_url, lat, long)
 VALUES (4, 'Local Art Galleries', 'Discovering new artists and artworks', 'https://example.com/images/art-galleries.jpg', -123.116944, 49.281389);
 
-INSERT INTO maps (owner_id, title, map_description, map_image, lat, long)
+INSERT INTO maps (owner_id, title, description, image_url, lat, long)
 VALUES (5, 'Ski Resorts', 'Snowy escapes for winter fun', 'https://example.com/images/ski-resorts.jpg', -123.116944, 49.281389);
