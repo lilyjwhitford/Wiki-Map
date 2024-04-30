be9572047fe20c8e110bafb46212fa420bdce9b0
PeterDevelops <PeterDinhBC@gmail.com>
2024-04-29 20:44:46 -0700

    style: remove unnecessary space

--- routes/login.js
+++ routes/login.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-
 router.get('/:id', (req, res) => {
   // set a cookie as a string value
   res.cookie('user_id', req.params.id);
