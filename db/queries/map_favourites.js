ca836aec09d39b4ee297c084c3f1e7e4fb1e1e98
PeterDevelops <PeterDinhBC@gmail.com>
2024-05-01 11:04:40 -0700

    chore: link jquery in map.ejs

---
+++ db/queries/map_favourites.js
@@ -0,0 +1,31 @@
+const db = require('../connection');
+
+const addFavourite = option => {
+  const { user_id, map_id } = option;
+
+  return db.query(`
+  INSERT INTO map_favourite (user_id, map_id) VALUES
+  ($1, $2) RETURNING *
+  ;`, [user_id, map_id])
+  .then(result => {
+    console.log('addFavourite Result: ',result);
+    return result.rows[0];
+  })
+  .catch(err => {
+    console.error(err);
+    throw new Error('Could not add to favourite')
+  });
+};
+
+const deleteFavourite = mapId => {
+  return db.query(`
+  DELETE FROM map_favourites WHERE map_id = $1
+  ;`, [mapId])
+  .then(result => {
+    console.log('Succesfully deleted');
+  })
+  .catch(err => {
+    console.error(err);
+    throw new Error('Could not delete map');
+  });
+};
