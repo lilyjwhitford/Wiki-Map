8699ca927c1ef9e687c5f7983e7b9a3b42e2eaf5
Lisa Wild <lisakwild@gmail.com>
2024-04-30 19:42:14 -0700

    bugfix: correct map data, long and lat to be central to the markers; image is the same while testing

--- db/seeds/02_maps.sql
+++ db/seeds/02_maps.sql
@@ -1,15 +1,21 @@
--- maps table seeds here (Example)
-INSERT INTO maps (owner_id, title, description, image_url, lat, long)
-VALUES (1, 'Best Sushi Spots', 'my favourite sushi places to bring my friends', 'https://images.unsplash.com/photo-1521722210400-244bfa6d1b1b', 34.052235, -118.243683);
+-- maps table seeds here 
 
+-- BEST SUSHI SPOTS
 INSERT INTO maps (owner_id, title, description, image_url, lat, long)
-VALUES (2, 'Top Hiking Trails', 'My favorite trails for a great adventure', 'https://images.unsplash.com/photo-1518791841217-8f162f1e1135', 51.5074, -0.1278);
+VALUES (1, 'Best Sushi Spots', 'my favourite sushi places to bring my friends', 'https://images.unsplash.com/photo-1521722210400-244bfa6d1b1b', 49.2616, -123.0756);
 
+-- TOP HIKING TRAILS
 INSERT INTO maps (owner_id, title, description, image_url, lat, long)
-VALUES (3, 'Best Coffee Shops', 'Spots for a cozy coffee break', 'https://images.unsplash.com/photo-1514888286974-6c03e2ca1dba', 40.7128, -74.0060);
+VALUES (2, 'Top Hiking Trails', 'My favorite trails for a great adventure', 'https://images.unsplash.com/photo-1518791841217-8f162f1e1135', 49.3052, -123.0064);
 
+-- BEST COFFEE SHOPS
 INSERT INTO maps (owner_id, title, description, image_url, lat, long)
-VALUES (4, 'Local Art Galleries', 'Discovering new artists and artworks', 'https://images.unsplash.com/photo-1536054106160-8a1b3a3e3e59', 45.5017, -73.5673);
+VALUES (3, 'Best Coffee Shops', 'Spots for a cozy coffee break', 'https://images.unsplash.com/photo-1514888286974-6c03e2ca1dba', 49.1946, -122.8828);
 
+-- LOCAL ART GALLERIES
 INSERT INTO maps (owner_id, title, description, image_url, lat, long)
-VALUES (5, 'Ski Resorts', 'Snowy escapes for winter fun', 'https://images.unsplash.com/photo-1520828697294-361b5f3afbe6', 49.2827, -123.1207);
+VALUES (4, 'Local Art Galleries', 'Discovering new artists and artworks', 'https://images.unsplash.com/photo-1536054106160-8a1b3a3e3e59', 49.2652, -122.8572);
+
+-- SKI RESORTS
+INSERT INTO maps (owner_id, title, description, image_url, lat, long)
+VALUES (5, 'Ski Resorts', 'Snowy escapes for winter fun', 'https://images.unsplash.com/photo-1520828697294-361b5f3afbe6', 49.88268, -122.93918);
--- db/seeds/03_markers.sql
+++ db/seeds/03_markers.sql
@@ -1,81 +1,81 @@
 -- markers table seeds here
 
 -- BEST SUSHI
-INSERT INTO markers (creator_id, map_id, long, lat, title, description, image_url)
-VALUES (2, 1, -123.114, 49.282, 'Sushi House', 'Cozy spot with fresh sushi rolls', 'https://images.unsplash.com/photo-1548011530-6dfacbacbf5f');
+INSERT INTO markers (creator_id, map_id, lat, long, title, description, image_url)
+VALUES (2, 1, 49.282, -123.117, 'Sushi House', 'Cozy spot with fresh sushi rolls', 'https://images.unsplash.com/photo-1563409236302-8442b5e644df?q=80&w=1888&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D');
 
-INSERT INTO markers (creator_id, map_id, long, lat, title, description, image_url)
-VALUES (3, 1, -123.117, 49.280, 'Ocean Blue Sushi', 'Ocean-themed restaurant with creative sushi dishes', 'https://images.unsplash.com/photo-1565299136858-5bfa5898757e');
+INSERT INTO markers (creator_id, map_id, lat, long, title, description, image_url)
+VALUES (3, 1, 49.282, -123.073, 'Ocean Blue Sushi', 'Ocean-themed restaurant with creative sushi dishes', 'https://images.unsplash.com/photo-1563409236302-8442b5e644df?q=80&w=1888&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D');
 
-INSERT INTO markers (creator_id, map_id, long, lat, title, description, image_url)
-VALUES (4, 1, -123.119, 49.285, 'Soy Sauce Sushi Bar', 'Authentic Japanese sushi experience', 'https://images.unsplash.com/photo-1579075576552-46c77acdf4bb');
+INSERT INTO markers (creator_id, map_id, lat, long, title, description, image_url)
+VALUES (4, 1, 49.204, -123.101, 'Soy Sauce Sushi Bar', 'Authentic Japanese sushi experience', 'https://images.unsplash.com/photo-1563409236302-8442b5e644df?q=80&w=1888&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D');
 
-INSERT INTO markers (creator_id, map_id, long, lat, title, description, image_url)
-VALUES (5, 1, -123.120, 49.283, 'Zen Garden Sushi', 'Tranquil atmosphere with a variety of sushi options', 'https://images.unsplash.com/photo-1597674601543-8f3c3603bc4c');
+INSERT INTO markers (creator_id, map_id, lat, long, title, description, image_url)
+VALUES (5, 1, 49.269, -123.069, 'Zen Garden Sushi', 'Tranquil atmosphere with a variety of sushi options', 'https://images.unsplash.com/photo-1563409236302-8442b5e644df?q=80&w=1888&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D');
 
-INSERT INTO markers (creator_id, map_id, long, lat, title, description, image_url)
-VALUES (1, 1, -123.116, 49.282, 'Sashimi Haven', 'Specializes in fresh sashimi cuts', 'https://images.unsplash.com/photo-1551963831-b3b1ca40c98e');
+INSERT INTO markers (creator_id, map_id, lat, long, title, description, image_url)
+VALUES (1, 1, 49.263, -123.068, 'Sashimi Haven', 'Specializes in fresh sashimi cuts', 'https://images.unsplash.com/photo-1563409236302-8442b5e644df?q=80&w=1888&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D');
 
 -- TOP HIKING TRAILS
-INSERT INTO markers (creator_id, map_id, long, lat, title, description, image_url)
-VALUES (1, 2, 34.051, -118.241, 'Mountain Vista Trail', 'Offers breathtaking views of the city skyline', 'https://images.unsplash.com/photo-1528901166001-34e6b35c6bce');
+INSERT INTO markers (creator_id, map_id, lat, long, title, description, image_url)
+VALUES (1, 2, 49.356, -123.078, 'Mountain Vista Trail', 'Offers breathtaking views of the city skyline', 'https://images.unsplash.com/photo-1563409236302-8442b5e644df?q=80&w=1888&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D');
 
-INSERT INTO markers (creator_id, map_id, long, lat, title, description, image_url)
-VALUES (3, 2, 34.055, -118.240, 'Wildflower Path', 'Scenic trail with colorful blooms in spring', 'https://images.unsplash.com/photo-1520437112468-6d98e31ea5a0');
+INSERT INTO markers (creator_id, map_id, lat, long, title, description, image_url)
+VALUES (3, 2, 49.306, -122.883, 'Wildflower Path', 'Scenic trail with colorful blooms in spring', 'https://images.unsplash.com/photo-1563409236302-8442b5e644df?q=80&w=1888&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D');
 
-INSERT INTO markers (creator_id, map_id, long, lat, title, description, image_url)
-VALUES (4, 2, 34.048, -118.239, 'River Canyon Trail', 'Trail following a serene river canyon', 'https://images.unsplash.com/photo-1550699560-007b25ae10d6');
+INSERT INTO markers (creator_id, map_id, lat, long, title, description, image_url)
+VALUES (4, 2, 49.344, -123.254, 'River Canyon Trail', 'Trail following a serene river canyon', 'https://images.unsplash.com/photo-1563409236302-8442b5e644df?q=80&w=1888&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D');
 
-INSERT INTO markers (creator_id, map_id, long, lat, title, description, image_url)
-VALUES (5, 2, 34.053, -118.244, 'Sunrise Summit Trail', 'Perfect for catching stunning sunrise views', 'https://images.unsplash.com/photo-1476716944581-ba51d74e84e7');
+INSERT INTO markers (creator_id, map_id, lat, long, title, description, image_url)
+VALUES (5, 2, 49.291, -123.018, 'Sunrise Summit Trail', 'Perfect for catching stunning sunrise views', 'https://images.unsplash.com/photo-1563409236302-8442b5e644df?q=80&w=1888&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D');
 
-INSERT INTO markers (creator_id, map_id, long, lat, title, description, image_url)
-VALUES (2, 2, 34.050, -118.245, 'Pine Grove Trail', 'Shaded path through a dense pine forest', 'https://images.unsplash.com/photo-1565938734658-9b4ec1f081f1');
+INSERT INTO markers (creator_id, map_id, lat, long, title, description, image_url)
+VALUES (2, 2, 49.229, -122.601, 'Pine Grove Trail', 'Shaded path through a dense pine forest', 'https://images.unsplash.com/photo-1563409236302-8442b5e644df?q=80&w=1888&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D');
 
 -- BEST COFFEE SHOPS
-INSERT INTO markers (creator_id, map_id, long, lat, title, description, image_url)
-VALUES (2, 3, 40.713, -74.005, 'Urban Brew', 'Hip coffee joint with industrial-chic decor', 'https://images.unsplash.com/photo-1502691876148-b89e6b3aafd6');
+INSERT INTO markers (creator_id, map_id, lat, long, title, description, image_url)
+VALUES (2, 3, 49.108, -122.672, 'Urban Brew', 'Hip coffee joint with industrial-chic decor', 'https://images.unsplash.com/photo-1563409236302-8442b5e644df?q=80&w=1888&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D');
 
-INSERT INTO markers (creator_id, map_id, long, lat, title, description, image_url)
-VALUES (3, 3, 40.710, -74.007, 'Morning Bliss Cafe', 'Cozy spot with delicious coffee and pastries', 'https://images.unsplash.com/photo-1597422541153-fba63d7258d6');
+INSERT INTO markers (creator_id, map_id, lat, long, title, description, image_url)
+VALUES (3, 3, 49.107, -122.649, 'Morning Bliss Cafe', 'Cozy spot with delicious coffee and pastries', 'https://images.unsplash.com/photo-1563409236302-8442b5e644df?q=80&w=1888&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D');
 
-INSERT INTO markers (creator_id, map_id, long, lat, title, description, image_url)
-VALUES (4, 3, 40.712, -74.006, 'Caffeine Fix', 'Popular coffee shop known for its strong brews', 'https://images.unsplash.com/photo-1554200864-0936cf9dbfcc');
+INSERT INTO markers (creator_id, map_id, lat, long, title, description, image_url)
+VALUES (4, 3, 49.280, -123.098, 'Caffeine Fix', 'Popular coffee shop known for its strong brews', 'https://images.unsplash.com/photo-1563409236302-8442b5e644df?q=80&w=1888&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D');
 
-INSERT INTO markers (creator_id, map_id, long, lat, title, description, image_url)
-VALUES (5, 3, 40.711, -74.008, 'Sip & Savor', 'Relaxed atmosphere perfect for enjoying a cup of coffee', 'https://images.unsplash.com/photo-1580538896803-39386b97ff36');
+INSERT INTO markers (creator_id, map_id, lat, long, title, description, image_url)
+VALUES (5, 3, 49.280, -123.114, 'Sip & Savor', 'Relaxed atmosphere perfect for enjoying a cup of coffee', 'https://images.unsplash.com/photo-1563409236302-8442b5e644df?q=80&w=1888&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D');
 
-INSERT INTO markers (creator_id, map_id, long, lat, title, description, image_url)
-VALUES (1, 3, 40.714, -74.004, 'Espresso Junction', 'Quaint cafe serving a variety of espresso drinks', 'https://images.unsplash.com/photo-1547038260-586b76b930f1');
+INSERT INTO markers (creator_id, map_id, lat, long, title, description, image_url)
+VALUES (1, 3, 49.298, -122.823, 'Espresso Junction', 'Quaint cafe serving a variety of espresso drinks', 'https://images.unsplash.com/photo-1563409236302-8442b5e644df?q=80&w=1888&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D');
 
 -- LOCAL ART GALLERIES
-INSERT INTO markers (creator_id, map_id, long, lat, title, description, image_url)
-VALUES (5, 4, 45.5015, -73.5675, 'Gallery One', 'Contemporary art gallery featuring local talent', 'https://images.unsplash.com/photo-1518128032248-77595fda9e5b');
+INSERT INTO markers (creator_id, map_id, lat, long, title, description, image_url)
+VALUES (5, 4, 49.326, -123.078, 'Gallery One', 'Contemporary art gallery featuring local talent', 'https://images.unsplash.com/photo-1563409236302-8442b5e644df?q=80&w=1888&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D');
 
-INSERT INTO markers (creator_id, map_id, long, lat, title, description, image_url)
-VALUES (1, 4, 45.5016, -73.5672, 'Canvas Collective', 'Exhibits a diverse range of paintings and sculptures', 'https://images.unsplash.com/photo-1565029763258-8df90146c0e0');
+INSERT INTO markers (creator_id, map_id, lat, long, title, description, image_url)
+VALUES (1, 4, 49.104, -122.644, 'Canvas Collective', 'Exhibits a diverse range of paintings and sculptures', 'https://images.unsplash.com/photo-1563409236302-8442b5e644df?q=80&w=1888&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D');
 
-INSERT INTO markers (creator_id, map_id, long, lat, title, description, image_url)
-VALUES (3, 4, 45.5018, -73.5671, 'Artisan Studio', 'Showcases local artisans crafts and handmade goods', 'https://images.unsplash.com/photo-1513755663523-7e4e3e6e8cac');
+INSERT INTO markers (creator_id, map_id, lat, long, title, description, image_url)
+VALUES (3, 4, 49.277, -122.861, 'Artisan Studio', 'Showcases local artisans crafts and handmade goods', 'https://images.unsplash.com/photo-1563409236302-8442b5e644df?q=80&w=1888&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D');
 
-INSERT INTO markers (creator_id, map_id, long, lat, title, description, image_url)
-VALUES (2, 4, 45.5014, -73.5674, 'Modern Art Loft', 'Experimental gallery pushing the boundaries of contemporary art', 'https://images.unsplash.com/photo-1530795517270-1f00d0336ec5');
+INSERT INTO markers (creator_id, map_id, lat, long, title, description, image_url)
+VALUES (2, 4, 49.226, -122.978, 'Modern Art Loft', 'Experimental gallery pushing the boundaries of contemporary art', 'https://images.unsplash.com/photo-1563409236302-8442b5e644df?q=80&w=1888&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D');
 
-INSERT INTO markers (creator_id, map_id, long, lat, title, description, image_url)
-VALUES (4, 4, 45.5019, -73.5673, 'Sculpture Haven', 'Focuses on three-dimensional art forms and installations', 'https://images.unsplash.com/photo-1519764622346-87b90fa02db6');
+INSERT INTO markers (creator_id, map_id, lat, long, title, description, image_url)
+VALUES (4, 4, 49.282, -122.531, 'Sculpture Haven', 'Focuses on three-dimensional art forms and installations', 'https://images.unsplash.com/photo-1563409236302-8442b5e644df?q=80&w=1888&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D');
 
--- SKI RESORTS
-INSERT INTO markers (creator_id, map_id, long, lat, title, description, image_url)
-VALUES (1, 5, 49.281, -123.121, 'Powder Peak Resort', 'Family-friendly resort with diverse slopes', 'https://images.unsplash.com/photo-1504279097621-33597d3a39c7');
+-- BEST SKI RESORTS
+INSERT INTO markers (creator_id, map_id, lat, long, title, description, image_url)
+VALUES (5, 5, 50.1161, -122.9574, 'Whistler Blackcomb', 'One of the largest ski resorts in North America', 'https://images.unsplash.com/photo-1563409236302-8442b5e644df?q=80&w=1888&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D');
 
-INSERT INTO markers (creator_id, map_id, long, lat, title, description, image_url)
-VALUES (2, 5, 49.284, -123.119, 'Alpine Adventure Park', 'Offers thrilling runs for experienced skiers', 'https://images.unsplash.com/photo-1518176258769-3258af8806b5');
+INSERT INTO markers (creator_id, map_id, lat, long, title, description, image_url)
+VALUES (5, 5, 49.4075, -123.2059, 'Cypress Mountain', 'Popular ski resort with stunning views of Vancouver', 'https://images.unsplash.com/photo-1563409236302-8442b5e644df?q=80&w=1888&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D');
 
-INSERT INTO markers (creator_id, map_id, long, lat, title, description, image_url)
-VALUES (3, 5, 49.283, -123.122, 'Snowy Summit Slopes', 'Panoramic views from the highest peak in the region', 'https://images.unsplash.com/photo-1489367874815-d3c91199357e');
+INSERT INTO markers (creator_id, map_id, lat, long, title, description, image_url)
+VALUES (5, 5, 49.3618, -123.0892, 'Grouse Mountain', 'Year-round destination for skiing, snowboarding, and more', 'https://images.unsplash.com/photo-1563409236302-8442b5e644df?q=80&w=1888&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D');
 
-INSERT INTO markers (creator_id, map_id, long, lat, title, description, image_url)
-VALUES (4, 5, 49.280, -123.120, 'Frosty Valley Resort', 'Ideal for beginners with gentle slopes and ski schools', 'https://images.unsplash.com/photo-1477322524743-0b4f6c71d51d');
+INSERT INTO markers (creator_id, map_id, lat, long, title, description, image_url)
+VALUES (5, 5, 50.0847, -123.0834, 'Mount Seymour', 'Family-friendly ski area offering a range of slopes', 'https://images.unsplash.com/photo-1563409236302-8442b5e644df?q=80&w=1888&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D');
 
-INSERT INTO markers (creator_id, map_id, long, lat, title, description, image_url)
-VALUES (5, 5, 49.282, -123.118, 'Glacier Ridge Retreat', 'Remote resort offering backcountry skiing adventures', 'https://images.unsplash.com/photo-1485483740779-28578357c4e0');
+INSERT INTO markers (creator_id, map_id, lat, long, title, description, image_url)
+VALUES (5, 5, 49.4432, -122.8927, 'Mount Strachan', 'Challenging terrain for advanced skiers and snowboarders', 'https://images.unsplash.com/photo-1563409236302-8442b5e644df?q=80&w=1888&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D');
