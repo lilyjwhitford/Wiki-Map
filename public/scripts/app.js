640d217a4d05f2dd7c96669bd7459328fe7e4d0a
Lisa Wild <lisakwild@gmail.com>
2024-05-01 22:36:14 -0700

    feat: enable marker to be deleted (jQuery, leaflet)

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,13 +4,14 @@
 // })
 
 const loadMap = (paramsObj) => {
-  const { lat, long, zoom, markers } = paramsObj;
+  const { lat, long, zoom, markers, mapID } = paramsObj;
   console.log('paramsObj -------', paramsObj);
   let map = L.map('map').setView([lat, long], zoom);
   L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
   }).addTo(map);
+
 
   // make loop to add marker to the map
   markers.forEach(data => {
@@ -23,7 +24,31 @@ const loadMap = (paramsObj) => {
     <br><img src="${data.image_url}" alt="Marker Image">
     <p>${data.description}</p>
     <button id="edit-marker-btn" class="btn btn-primary">Edit</button>
-    <button id="delete-marker-btn" class="btn btn-danger">Delete</button>`).openPopup();
+    <button id="delete-marker-btn" class="${data.id}">Delete</button>`);
+
   });
+
+  map.on('popupopen', function () {
+    $('#delete-marker-btn').click(function (e) {
+      e.preventDefault();
+      // get markerID from buttons data attribute
+      console.log(e);
+      let markerID = e.target.className;
+      // let mapID = e.
+      console.log(markerID);
+      // // post request to delete the marker
+      $.post(`/api/maps/${mapID}/markers/${markerID}/delete`)
+        .then((response) => { console.log(response)      
+          // $.get(`/api/maps/${mapID}`)              //with this uncommented it messes with the add marker
+          //   .then(mapDataResponse => {
+          //     // remove current map on page
+          //     map.remove();
+          //     // add all markers, including the new marker, to the map
+          //     loadMap(mapDataResponse);
+            // })
+        });
+    });
+  })
+
   return map;
 };
