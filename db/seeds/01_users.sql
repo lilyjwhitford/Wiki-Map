90e0a6be42f66c74beaab8169996620f9a538a20
Lisa Wild <lisakwild@gmail.com>
2024-05-02 23:45:47 -0700

    bugfix: update image_url with working images

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,15 +1,15 @@
 -- Users table seeds here (Example)
 INSERT INTO users (username, email, password, profile_photo_url, bio)
-VALUES ('MapLover10', 'pirate@gmail.com', 'password', 'https://easydrawingguides.com/wp-content/uploads/2021/06/how-to-draw-a-pirate-featured-image-1200.png', 'some text');
+VALUES ('@MapLover10', 'pirate@gmail.com', 'password', 'https://easydrawingguides.com/wp-content/uploads/2021/06/how-to-draw-a-pirate-featured-image-1200.png', 'Charting a course for uncharted riches, one rum-stained map at a time.');
 
 INSERT INTO users (username, email, password, profile_photo_url, bio)
-VALUES ('TechGuru42', 'alice@yahoo.com', 'password', 'https://example.com/images/tech-guru.jpg', 'Passionate about technology and innovation.');
+VALUES ('@TechGuru42', 'alice@yahoo.com', 'password', 'https://images.unsplash.com/photo-1592188657297-c6473609e988?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8bmVyZCUyMGdpcmx8ZW58MHwxfDB8fHwy', 'Passionate about technology and innovation.');
 
 INSERT INTO users (username, email, password, profile_photo_url, bio)
-VALUES ('FoodieQueen', 'bob@example.com', 'password', 'https://example.com/images/foodie-queen.jpg', 'Love exploring new recipes and sharing culinary adventures.');
+VALUES ('@FoodieQueen', 'bob@example.com', 'password', 'https://images.unsplash.com/photo-1494790108377-be9c29b29330?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cHJvZmlsZSUyMHBob3RvfGVufDB8MXwwfHx8Mg%3D%3D', 'Love exploring new recipes and sharing culinary adventures.');
 
 INSERT INTO users (username, email, password, profile_photo_url, bio)
-VALUES ('TravelJunkie', 'charlie@travel.com', 'password', 'https://example.com/images/travel-junkie.jpg', 'Constantly on the move, exploring the world one destination at a time.');
+VALUES ('@TravelJunkie', 'charlie@travel.com', 'password', 'https://images.unsplash.com/photo-1548280684-8c051518e195?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8cHJvZmlsZSUyMHBob3RvfGVufDB8MXwwfHx8Mg%3D%3D', 'Constantly on the move, exploring the world one destination at a time.');
 
 INSERT INTO users (username, email, password, profile_photo_url, bio)
-VALUES ('FitnessFanatic', 'dana@fitness.com', 'password', 'https://example.com/images/fitness-fanatic.jpg', 'Dedicated to a healthy lifestyle and fitness goals.');
+VALUES ('@FitnessFanatic', 'dana@fitness.com', 'password', 'https://images.unsplash.com/photo-1614289371518-722f2615943d?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fHByb2ZpbGUlMjBwaG90b3xlbnwwfDF8MHx8fDI%3D', 'Dedicated to a healthy lifestyle and fitness goals.');
