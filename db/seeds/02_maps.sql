c3afee0d06e9062fbc8b5a7a20f7f786000ac204
PeterDevelops <PeterDinhBC@gmail.com>
2024-05-03 18:35:16 -0700

    refactor: change some inserts

--- db/seeds/02_maps.sql
+++ db/seeds/02_maps.sql
@@ -1,21 +1,41 @@
 -- maps table seeds here
 
--- BEST SUSHI SPOTS
+-- BEST POTATO SPOTS
 INSERT INTO maps (owner_id, title, description, image_url, lat, long)
-VALUES (1, 'Best Sushi Spots', 'my favourite sushi places to bring my friends', 'https://images.squarespace-cdn.com/content/v1/63f52cb70db2e54d5c3fa9c6/fd088cf1-c2d9-4f1d-b179-72286bba52b1/IMG_6093.jpg', 49.2616, -123.0756);
+VALUES (1, 'Best Potatoes in Town', 'my favourite sushi places to bring my friends', 'https://images.squarespace-cdn.com/content/v1/63f52cb70db2e54d5c3fa9c6/fd088cf1-c2d9-4f1d-b179-72286bba52b1/IMG_6093.jpg', 49.2616, -123.0756);
 
 -- TOP HIKING TRAILS
 INSERT INTO maps (owner_id, title, description, image_url, lat, long)
-VALUES (2, 'Top Hiking Trails', 'My favorite trails for a great adventure', 'https://media.timeout.com/images/105898986/image.jpg', 49.3052, -123.0064);
+VALUES (2, 'Top Potato Farms', 'My favorite trails for a great adventure', 'https://media.timeout.com/images/105898986/image.jpg', 49.3052, -123.0064);
 
 -- BEST COFFEE SHOPS
 INSERT INTO maps (owner_id, title, description, image_url, lat, long)
-VALUES (3, 'Best Coffee Shops', 'Spots for a cozy coffee break', 'https://us.123rf.com/450wm/mergerr92/mergerr922401/mergerr92240110607/222885349-coffee-cup-on-wooden-table-in-coffee-shop-stock-photo.jpg?ver=6', 49.1946, -122.8828);
+VALUES (3, 'Local Potato Festivals', 'Spots for a cozy coffee break', 'https://us.123rf.com/450wm/mergerr92/mergerr922401/mergerr92240110607/222885349-coffee-cup-on-wooden-table-in-coffee-shop-stock-photo.jpg?ver=6', 49.1946, -122.8828);
 
 -- LOCAL ART GALLERIES
 INSERT INTO maps (owner_id, title, description, image_url, lat, long)
-VALUES (4, 'Local Art Galleries', 'Discovering new artists and artworks', 'https://images.pexels.com/photos/69903/pexels-photo-69903.jpeg', 49.2652, -122.8572);
+VALUES (4, '', 'Discovering new artists and artworks', 'https://images.pexels.com/photos/69903/pexels-photo-69903.jpeg', 49.2652, -122.8572);
 
 -- SKI RESORTS
 INSERT INTO maps (owner_id, title, description, image_url, lat, long)
 VALUES (5, 'Ski Resorts', 'Snowy escapes for winter fun', 'https://t3.ftcdn.net/jpg/00/49/67/28/360_F_49672885_AgQCuPEw3jLfp5TvlZghqd8JFkRERszR.jpg', 49.88268, -122.93918);
+
+-- -- BEST SUSHI SPOTS
+-- INSERT INTO maps (owner_id, title, description, image_url, lat, long)
+-- VALUES (1, 'Best Sushi Spots', 'my favourite sushi places to bring my friends', 'https://images.squarespace-cdn.com/content/v1/63f52cb70db2e54d5c3fa9c6/fd088cf1-c2d9-4f1d-b179-72286bba52b1/IMG_6093.jpg', 49.2616, -123.0756);
+
+-- -- TOP HIKING TRAILS
+-- INSERT INTO maps (owner_id, title, description, image_url, lat, long)
+-- VALUES (2, 'Top Hiking Trails', 'My favorite trails for a great adventure', 'https://media.timeout.com/images/105898986/image.jpg', 49.3052, -123.0064);
+
+-- -- BEST COFFEE SHOPS
+-- INSERT INTO maps (owner_id, title, description, image_url, lat, long)
+-- VALUES (3, 'Best Coffee Shops', 'Spots for a cozy coffee break', 'https://us.123rf.com/450wm/mergerr92/mergerr922401/mergerr92240110607/222885349-coffee-cup-on-wooden-table-in-coffee-shop-stock-photo.jpg?ver=6', 49.1946, -122.8828);
+
+-- -- LOCAL ART GALLERIES
+-- INSERT INTO maps (owner_id, title, description, image_url, lat, long)
+-- VALUES (4, 'Local Art Galleries', 'Discovering new artists and artworks', 'https://images.pexels.com/photos/69903/pexels-photo-69903.jpeg', 49.2652, -122.8572);
+
+-- -- SKI RESORTS
+-- INSERT INTO maps (owner_id, title, description, image_url, lat, long)
+-- VALUES (5, 'Ski Resorts', 'Snowy escapes for winter fun', 'https://t3.ftcdn.net/jpg/00/49/67/28/360_F_49672885_AgQCuPEw3jLfp5TvlZghqd8JFkRERszR.jpg', 49.88268, -122.93918);
