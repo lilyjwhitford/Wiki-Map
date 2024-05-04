1e42a903e76cab7bfdd7328c977309c4c9057d01
Lily Whitford <lilywhitford2@gmail.com>
2024-05-04 02:04:31 +0000

    feat: style marker pop up

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -19,11 +19,13 @@ const loadMap = (paramsObj) => {
     let marker = L.marker([data.lat, data.long]).addTo(map);
     // add a pop-up to the marker
     marker.bindPopup(`
-    <b>${data.title}</b>
-    <br><img src="${data.image_url}" alt="Marker Image">
-    <p>${data.description}</p>
-    <button id="edit-marker-btn" data-marker-id="${data.id}" class="btn btn-edit">Edit</button>
-    <button id="delete-marker-btn" data-marker-id="${data.id}" class="btn btn-danger">Delete</button>`);
+    <div class="popup-container">
+      <b class="marker-title">${data.title}</b>
+      <br><img src="${data.image_url}" alt="Marker Image" class="popup-img">
+      <p class="marker-description">${data.description}</p>
+      <button id="edit-marker-btn" data-marker-id="${data.id}" class="btn btn-edit">Edit</button>
+      <button id="delete-marker-btn" data-marker-id="${data.id}" class="btn btn-danger">Delete</button>
+    </div>`);
   });
   return map;
 };
